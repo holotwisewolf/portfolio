40a3b4b3bede6ba80cadc486e32a5771a89d641f
YJ <choongyj-wm24@student.tarc.edu.my>
2026-05-31 06:10:05 -0400

    feat: build Welcome window

--- components/windows/Welcome.tsx
+++ components/windows/Welcome.tsx
@@ -1,8 +1,31 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 export default function Welcome() {
+  const [text, setText] = useState('')
+  const fullText = '> initializing portfolio...'
+
+  useEffect(() => {
+    let i = 0
+    const timer = setInterval(() => {
+      if (i < fullText.length) {
+        setText(fullText.slice(0, i + 1))
+        i++
+      } else {
+        clearInterval(timer)
+      }
+    }, 50)
+    return () => clearInterval(timer)
+  }, [])
+
   return (
     <div className="h-full flex flex-col justify-center items-center gap-8">
       <h1 className="text-4xl font-bold">PORTFOLIO</h1>
-      <div className="text-lg font-mono">Welcome window - to be implemented</div>
+      <div className="text-lg font-mono">{text}</div>
+      <div className="text-sm opacity-70">
+        Double-click desktop icons to open windows
+      </div>
     </div>
   )
 }
