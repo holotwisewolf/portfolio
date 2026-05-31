0921e523b461cee033a33ce74a344b821e42ce0c
YJ <choongyj-wm24@student.tarc.edu.my>
2026-05-31 06:11:33 -0400

    feat: auto-open Welcome window on load

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
 'use client'
 
+import { useEffect } from 'react'
 import Desktop from '@/components/window-manager/Desktop'
 import Taskbar from '@/components/window-manager/Taskbar'
-import { WindowProvider } from '@/components/window-manager/useWindows'
+import { WindowProvider, useWindowStore } from '@/components/window-manager/useWindows'
+import Welcome from '@/components/windows/Welcome'
+
+function AppContent() {
+  const openWindow = useWindowStore((s) => s.openWindow)
+
+  useEffect(() => {
+    // Auto-open welcome window on load
+    openWindow('welcome', 'Welcome', Welcome)
+  }, [openWindow])
+
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-black">
+      <Desktop />
+      <Taskbar />
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <WindowProvider>
-      <div className="h-screen w-screen overflow-hidden bg-black">
-        <Desktop />
-        <Taskbar />
-      </div>
+      <AppContent />
     </WindowProvider>
   )
 }
